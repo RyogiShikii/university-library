3243ba9301042228db005cc31f2b54b545555e91
Daniel <guosongraiden@gmail.com>
2025-04-29 00:01:54 -0700

    complete home page

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Book } from "@/types";
+import React from "react";
+import BookCard from "./BookCard";
 
-const BookList = () => {
-  return (
-    <div>BookList</div>
-  )
+interface props {
+  title: string;
+  books: Book[];
+  containerClassName?: string;
 }
 
-export default BookList
+const BookList = ({title, books, containerClassName}: props) => {
+  return (
+    <section className={containerClassName}>
+      <h2 className=" font-bebas-neue text-4xl text-light-100 ">
+        {title}
+      </h2>
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default BookList;
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -48,7 +48,7 @@ const BookOverview = ({
 
         <p className="book-description">{description}</p>
 
-        <Button className="book-overview_btn cursor-pointer bg-[#E7C9A5] hover:bg-[#E7C9A5]/90">
+        <Button className="book-overview_btn">
           <Image src="/icons/book.svg" alt="book" width={20} height={20} />
           <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
         </Button>
@@ -63,11 +63,7 @@ const BookOverview = ({
             coverImage={cover}
           />
           <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
-          <BookCover
-            variant="wide"
-            coverColor={color}
-            coverImage={cover}
-          />
+            <BookCover variant="wide" coverColor={color} coverImage={cover} />
           </div>
         </div>
       </div>
