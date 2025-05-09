df70dd821116f3bf256201d24caf8bd330e306f4
Daniel <guosongraiden@gmail.com>
2025-05-08 23:58:02 -0700

    add redirect

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,18 @@
 import React, { ReactNode } from "react";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 import Header from "@/components/Header";
 
-const layout = ({ children }: { children: ReactNode }) => {
+const layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+
+  if (!session) redirect("/sign-in");
+
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
-        <Header />
+        <Header session={session} />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import React from "react";
-
+import { Session } from "next-auth";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-const Header = () => {
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { getInitials } from "@/lib/utils";
+
+
+const Header = ({session}: {session: Session}) => {
   const pathname = usePathname();
 
   return (
@@ -18,11 +22,21 @@ const Header = () => {
 
       <ul className="flex flex-row items-center gap-8">
         <li>
-          <Link href="/library" className={cn(
-            "text-base cursor-pointer capitalize",
-            pathname === "/library" ? "text-light-200" : "text-light-100",
-          )}>
+          <Link
+            href="/library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
             Library
+          </Link>
+        </li>
+        <li>
+          <Link href="/my-profile">
+            <Avatar>
+              <AvatarFallback className="bg-amber-100">{getInitials(session?.user?.name || "IN")}</AvatarFallback>
+            </Avatar>
           </Link>
         </li>
       </ul>
