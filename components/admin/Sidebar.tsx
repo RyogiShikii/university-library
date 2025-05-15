4ec91c1f48f650a374138da2d3cb9887abf0bcb0
Daniel Guo <guosongraiden@gmail.com>
2025-05-15 16:33:44 -0700

    initial admin page

---
+++ components/admin/Sidebar.tsx
@@ -0,0 +1,46 @@
+import { adminSideBarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { cn } from "@/lib/utils";
+
+const Sidebar = () => {
+  return (
+    <div className="admin-sidebar">
+      <div>
+        <div className="logo">
+          <Image
+            src="/icons/admin/logo.svg"
+            alt="logo"
+            height={37}
+            width={37}
+          />
+          <h1>BookWise</h1>
+        </div>
+
+        <div className="mt-10 flex flex-col gap-5">
+          {adminSideBarLinks.map((link) => {
+            const isSelected = false;
+
+            return (
+              <Link href={link.route} key={link.route}>
+                <div
+                  className={cn(
+                    "link",
+                    isSelected && "bg-primary-admin shadow-sm"
+                  )}
+                >
+                    <div className="relative size-5">
+                        <Image src={link.img} alt="icon" fill />
+                    </div>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
